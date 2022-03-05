66867556be95df17c34d09dc13bedea64fa72a58
Gongdongjang <jeesoo0908@gmail.com>
2022-03-05 23:46:43 +0900

    [기능 추가] 안드로이드 기본 세팅

---
+++ app/src/main/java/com/example/consumer_client/user/StartActivity.java
@@ -0,0 +1,15 @@
+package com.example.consumer_client.user;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.consumer_client.R;
+
+public class StartActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
