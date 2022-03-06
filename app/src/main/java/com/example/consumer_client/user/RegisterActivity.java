9e10a5574faf370b5457e1de5efe2f0ba600c6d7
Gongdongjang <jeesoo0908@gmail.com>
2022-03-06 17:27:24 +0900

    [기타] RegisterActivity + xml파일 수정

--- app/src/main/java/com/example/consumer_client/user/RegisterActivity.java
+++ app/src/main/java/com/example/consumer_client/user/RegisterActivity.java
@@ -1,15 +1,30 @@
 package com.example.consumer_client.user;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.consumer_client.R;
 
 public class RegisterActivity extends AppCompatActivity {
+    Button signup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //회원가입 버튼
+        signup = findViewById(R.id.signupbutton);
+
+        //회원가입 버튼 클릭시, 회원가입 페이지로 이동
+        signup.setOnClickListener(v -> {
+            Intent intent = new Intent(this, StartActivity.class);
+            startActivity(intent);
+        });
     }
+
 }
