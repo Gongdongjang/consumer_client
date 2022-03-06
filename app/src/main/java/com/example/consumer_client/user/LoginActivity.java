e25b63ae0f4782cad612644ae0aa222e073a5f9e
Suanna01 <yangnony01@gmail.com>
2022-03-06 16:48:47 +0900

    [기타] LoginActivity+ xml 파일 수정

--- app/src/main/java/com/example/consumer_client/user/LoginActivity.java
+++ app/src/main/java/com/example/consumer_client/user/LoginActivity.java
@@ -1,15 +1,28 @@
 package com.example.consumer_client.user;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.consumer_client.R;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView sign;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //회원가입 버튼
+        sign = findViewById(R.id.signin);
+
+        //회원가입 버튼 클릭시, 회원가입 페이지로 이동
+        sign.setOnClickListener(v -> {
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+        });
     }
 }
