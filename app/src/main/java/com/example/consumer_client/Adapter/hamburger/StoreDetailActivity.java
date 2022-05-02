3a87801435ee38ee55ca0828fcaba77f529938f7
Suanna01 <yangnony01@gmail.com>
2022-05-03 03:55:05 +0900

    [기능추가] 진행중인공동구매 프론트

--- app/src/main/java/com/example/consumer_client/Adapter/hamburger/StoreDetailActivity.java
+++ app/src/main/java/com/example/consumer_client/Adapter/hamburger/StoreDetailActivity.java
@@ -1,7 +1,10 @@
 package com.example.consumer_client.Adapter.hamburger;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,6 +30,16 @@ public class StoreDetailActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_detail);
+
+        //진행중인공동구매 이동
+        TextView textViewJP = (TextView) findViewById(R.id.JP);
+        textViewJP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), JointPurchaseActivity.class);
+                startActivity(intent);
+            }
+        });
 
         mContext = this;
 
