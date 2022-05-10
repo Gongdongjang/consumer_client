925833b17032def30e07de9cbc26b5a4e8b66d55
leejw-lu <jiwoojenny@naver.com>
2022-05-10 17:10:46 +0900

    [기능추가] 전체입점농가 리사이클러뷰 + 세부페이지 보이기

--- app/src/main/java/com/example/consumer_client/Adapter/hamburger/FarmDetailActivity.java
+++ app/src/main/java/com/example/consumer_client/Adapter/hamburger/FarmDetailActivity.java
@@ -1,7 +1,10 @@
 package com.example.consumer_client.Adapter.hamburger;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.consumer_client.R;
+
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
 
 import java.util.ArrayList;
 
@@ -26,9 +32,39 @@ public class FarmDetailActivity extends AppCompatActivity {
 
         mContext = this;
 
-        firstInit();
+        // 이제 햄버거 전체입점 농가보기 -> 공동장에서 만날수 있는 모든 농가 리사이클러뷰에서 농장 클릭-> 하면 보이는
+        // 세부페이지 정보 보이게 하기!!
 
+        //intent로 값 넘길때
+        Intent intent;
+        String farm_name, farm_info, farm_loc;
+
+        intent=getIntent(); //intent 값 받기
+
+        farm_name=intent.getStringExtra("farmName");
+        farm_info=intent.getStringExtra("farmInfo");
+        farm_loc=intent.getStringExtra("farmLoc");
+
+        TextView FarmName = (TextView) findViewById(R.id.FarmName);
+        TextView FarmExplain = (TextView) findViewById(R.id.FarmExplain);
+        TextView FarmLocation = (TextView) findViewById(R.id.FarmLocation);
+
+        FarmName.setText(farm_name);
+        FarmExplain.setText(farm_info);
+        FarmLocation.setText(farm_loc);
+
+        //지도
+        MapView mapView = new MapView(mContext);
+        // 중심점 변경 (농가위치 받아오면 위도, 경도 띄우게 하기) // 일단 나주농장 되는지 확인
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(35.01426900000000, 126.7169940), true);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.farm_map_view);
+        mapViewContainer.addView(mapView);
+
+
+        //----------세부페이지에 있는 진행중인 공동구매 리사이클러뷰 띄우게하기
         //추후에 제품 이름 가져올 예정
+        firstInit();
         for(int i=0;i<10;i++){
             addFarmJointPurchase("product Img", "농가 이름" + i, "농가 제품 이름" + i, "농가 특징" + i, "" + i);
         }
@@ -41,7 +77,7 @@ public class FarmDetailActivity extends AppCompatActivity {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(mContext);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(linearLayoutManager);
-}
+    }
 
     public void firstInit(){
         mRecyclerView = findViewById(R.id.FarmJointPurchaseView);
---
+++ app/src/main/java/com/example/consumer_client/FarmGet.java
@@ -0,0 +1,75 @@
+package com.example.consumer_client;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class FarmGet {
+
+    @SerializedName("farm_arr")
+    private List farm_arr;
+
+    @SerializedName("farm_mainItem")
+    private List farm_mainItem;
+
+    @SerializedName("farm_info")
+    private List farm_info;
+
+    @SerializedName("farm_loc")
+    private List farm_loc;
+
+    private int code;
+    public int getCode(){
+        return code;
+    }
+    public void setCode(){
+        this.code = code;
+    }
+
+    private String message;
+    public String getMessage(){
+        return message;
+    }
+    public void setMessage(){
+        this.message = message;
+    }
+
+    //농가이름
+    public List getFarm_arr(){ return farm_arr; }
+    public void setFarm_arr(){
+        this.farm_arr = farm_arr;
+    }
+    //농가 판매 물품
+    public List getFarm_mainItem(){
+        return farm_mainItem;
+    }
+    public void setFarm_mainItem(){
+        this.farm_mainItem = farm_mainItem;
+    }
+    //농가info
+    public List getFarm_info(){
+        return farm_info;
+    }
+    public void setFarm_info(){
+        this.farm_info = farm_info;
+    }
+
+    //농가위치
+    public List getFarm_loc(){
+        return farm_loc;
+    }
+    public void setFarm_loc(){
+
+        this.farm_loc = farm_loc;
+    }
+
+    @SerializedName("count")
+    private String count;
+    public String getCount(){
+        return count;
+    }
+    public void setCount(){
+        this.count = count;
+    }
+
+}
