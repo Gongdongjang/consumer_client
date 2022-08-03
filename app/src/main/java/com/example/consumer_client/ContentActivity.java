2fbc826e994f3f9d387ea992d208bb715a640012
eunjungL <dldmswjd322@gmail.com>
2022-08-03 23:51:38 +0900

    [기능추가] 콘텐츠 리스트 불러오기 추가

--- app/src/main/java/com/example/consumer_client/ContentActivity.java
+++ app/src/main/java/com/example/consumer_client/ContentActivity.java
@@ -3,11 +3,40 @@ package com.example.consumer_client;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
+interface ContentService {
+    @GET("content")
+    Call<ResponseBody> get_content();
+}
+
 public class ContentActivity extends AppCompatActivity {
+    String TAG = ContentActivity.class.getSimpleName();
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:5000/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+    ContentService contentService = retrofit.create(ContentService.class);
+    JsonParser jsonParser = new JsonParser();
 
     ListView content_list;
     ContentListAdapter contentListAdapter;
@@ -26,10 +55,36 @@ public class ContentActivity extends AppCompatActivity {
     }
 
     void get_content_list() {
-        // sample code for test
-        for (int i = 0; i < 5; i++) {
-            photo_urls.add("https://static.remove.bg/remove-bg-web/bf3af3e882eb04971b4492a1015ef7e77df29362/assets/start_remove-c851bdf8d3127a24e2d137a55b1b427378cd17385b01aec6e59d5d4b5f39d2ec.png");
-        }
-        contentListAdapter.notifyDataSetChanged();
+        Call<ResponseBody> call = contentService.get_content();
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    try {
+                        JsonArray res = (JsonArray) jsonParser.parse(response.body().string());
+                        for (int i = 0; i < res.size(); i++) {
+                            JsonObject jsonRes = (JsonObject) res.get(i);
+                            String photo_url = "https://gdjang.s3.ap-northeast-2.amazonaws.com/" + jsonRes.get("content_photo").toString().replaceAll("\"", "");
+                            photo_urls.add(photo_url);
+                           }
+                        Log.d(TAG, photo_urls.toString());
+                        contentListAdapter.notifyDataSetChanged();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    try {
+                        Log.d(TAG, "Fail " + response.errorBody().string());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
     }
 }
