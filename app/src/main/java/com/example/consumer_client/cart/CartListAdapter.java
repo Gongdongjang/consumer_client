290a08ed185ed1e00586d640456a3e901b65d516
Suanna01 <yangnony01@gmail.com>
2023-02-28 02:57:48 +0900

    #43 [기능추가] 장바구니 +,- 기능

--- app/src/main/java/com/example/consumer_client/cart/CartListAdapter.java
+++ app/src/main/java/com/example/consumer_client/cart/CartListAdapter.java
@@ -36,6 +36,9 @@ interface CartAdapterService{
     Call<ResponseBody> cartDelete(@Query("user_id") String user_id, @Query("store_id") String store_id, @Query("md_id") String md_id);
     @GET("/cartChecked")
     Call<ResponseBody> cartChecked(@Query("user_id") String user_id, @Query("row_num") int row_num);
+    @GET("/cartUpdate")
+    Call<ResponseBody> cartUpdate(@Query("select_qty") int select_qty, @Query("user_id") String user_id, @Query("store_id") String store_id, @Query("md_id") String md_id);
+
 }
 
 public class CartListAdapter extends RecyclerView.Adapter<CartListAdapter.ViewHolder> {
@@ -63,10 +66,10 @@ public class CartListAdapter extends RecyclerView.Adapter<CartListAdapter.ViewHo
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         ImageView mdImg;
-        TextView eachMdPrice, storeName, eachStoreTotalPrice, mdName, mdSet, qty;
+        TextView storeName, eachStoreTotalPrice, mdName, mdSet, qty;
         String TAG = CartListActivity.class.getSimpleName();
         Button deleteBtn;
-        JsonArray cart_checked;
+        JsonArray cart_checked, cart_update;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -90,7 +93,7 @@ public class CartListAdapter extends RecyclerView.Adapter<CartListAdapter.ViewHo
                     }
                 }
             });
-            eachMdPrice = (TextView) itemView.findViewById(R.id.CL_EachMdPrice);
+//            eachMdPrice = (TextView) itemView.findViewById(R.id.CL_EachMdPrice);
             eachStoreTotalPrice = (TextView) itemView.findViewById(R.id.CL_EachStoreTotalPrice);
             mdImg = (ImageView) itemView.findViewById(R.id.CL_MD_IMG);
             storeName = (TextView) itemView.findViewById(R.id.CL_StoreName);
@@ -98,6 +101,10 @@ public class CartListAdapter extends RecyclerView.Adapter<CartListAdapter.ViewHo
             mdSet = (TextView) itemView.findViewById(R.id.CL_MdSet);
             qty = (TextView) itemView.findViewById(R.id.CL_PurchaseNum);
             ImageView checkBtn = (ImageView) itemView.findViewById(R.id.CL_CheckButton);
+
+            ImageView mdMinusBtn = (ImageView) itemView.findViewById(R.id.CL_mdMinusBtn);
+            ImageView mdPlusBtn = (ImageView) itemView.findViewById(R.id.CL_mdPlusBtn);
+
             checkBtn.setImageResource(R.drawable.ic_check_off);
             checkBtn.setTag("check");
             checkBtn.setOnClickListener(new View.OnClickListener() {
@@ -174,108 +181,125 @@ public class CartListAdapter extends RecyclerView.Adapter<CartListAdapter.ViewHo
                 }
             });
 
-//            Call<ResponseBody> call = service.cartListGet();
-//            call.enqueue(new Callback<ResponseBody>() {
-//                @Override
-//                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-//                    try {
-//                        JsonObject res = (JsonObject) jsonParser.parse(response.body().string());
-//                        stk_remain = res.get("stk_remain").getAsJsonArray();
-//                        pay_price = res.get("pay_price").getAsJsonArray();
-//                        checkBtn.setOnClickListener(new View.OnClickListener() {
-//                            @Override
-//                            public void onClick(View view) {
-//                                if (checkBtn.getTag().equals("check")) {
-//                                    if (checked_count == 0) {
-//                                        checkBtn.setImageResource(R.drawable.ic_check_on);
-//                                        checkBtn.setTag("checked");
-//                                        checked_count += 1;
-//                                    } else {
-//                                        Toast.makeText(view.getContext(), "하나의 스토어의 제품만 선택할 수 있습니다.", Toast.LENGTH_LONG).show();
-//                                    }
-//                                } else {
-//                                    checkBtn.setImageResource(R.drawable.ic_check_off);
-//                                    checkBtn.setTag("check");
-//                                    checked_count -= 1;
-//                                }
-//                            }
-//                        });
-////                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//                @Override
-//                public void onFailure(Call<ResponseBody> call, Throwable t) {
-//                    Log.e(TAG, "onFailure: e " + t.getMessage());
-//                }
-//            });
+            mdPlusBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = getAdapterPosition();
+                    if (pos!=RecyclerView.NO_POSITION) {
+                        if (mListener != null) {
+                            pos_item = pos;
+                        }
+                    }
+                    Call<ResponseBody> call = service.cartChecked(user_id, pos);
+                    call.enqueue(new Callback<ResponseBody>() {
+                        @Override
+                        public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                            int count = 0;
+                            String store_id_pos = "", md_id_pos= "";
+                            try {
+                                JsonObject res = (JsonObject) jsonParser.parse(response.body().string());
+                                cart_checked = res.get("cart_checked").getAsJsonArray();
+                                count = cart_checked.get(0).getAsJsonObject().get("select_qty").getAsInt();
+                                int stk_remain = cart_checked.get(0).getAsJsonObject().get("stk_remain").getAsInt();
+                                int each_price = cart_checked.get(0).getAsJsonObject().get("pay_price").getAsInt();
+                                store_id_pos = cart_checked.get(0).getAsJsonObject().get("store_id").getAsString();
+                                md_id_pos = cart_checked.get(0).getAsJsonObject().get("md_id").getAsString();
 
-//                    Call<ResponseBody> call = service.cartListGet(body);
-//                    call.enqueue(new Callback<ResponseBody>() {
-//                        @Override
-//                        public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-//                            try {
-//                                JsonObject res = (JsonObject) jsonParser.parse(response.body().string());
-//                                stk_remain = res.get("stk_remain").getAsJsonArray();
-//                                pay_price = res.get("pay_price").getAsJsonArray();
-//                                if (checkBtn.getTag().equals("like")) {
-//                                    checkBtn.setImageResource(R.drawable.ic_check_on);
-//                                    checkBtn.setTag("liked");
-//                                } else {
-//                                    checkBtn.setImageResource(R.drawable.ic_check_off);
-//                                    checkBtn.setTag("like");
-//                                }
-//                            } catch (IOException e) {
-//                                e.printStackTrace();
-//                            }
-//                        }
-//                        @Override
-//                        public void onFailure(Call<ResponseBody> call, Throwable t) {
-//                            Log.e(TAG, "onFailure: e " + t.getMessage());
-//                        }
-//                    });
-//                }
-//            });
+                                if (stk_remain < (count+1) ){ //n세트 * m개
+                                    Toast.makeText(v.getContext(), "재고 확인 후 다시 주문해주세요", Toast.LENGTH_SHORT).show();
+                                } else{
+                                    count++;
+                                    qty.setText(count+"");
+                                    qty.setText(qty.getText().toString());
+                                    eachStoreTotalPrice.setText(String.valueOf(count * each_price));
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
 
-//            ImageView mdMinusBtn = (ImageView) itemView.findViewById(R.id.CL_mdMinusBtn);
-//            ImageView mdPlusBtn = (ImageView) itemView.findViewById(R.id.CL_mdPlusBtn);
-//
-//            //상품개수
-//            count=Integer.parseInt(String.valueOf(qty.getText()));
-//            Log.d("count : ", String.valueOf(count));
-//
-//            //+버튼
-//            mdPlusBtn.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    //재고 있을 시 count++
-//                    Log.d("count 개수: ", String.valueOf(count));
-////                    if(Integer.parseInt(StkRemain) < (count+1) ){ //n세트 * m개
-////                        Toast.makeText(v.getContext(), "재고 확인 후 다시 주문해주세요", Toast.LENGTH_SHORT).show();
-////                    }else{
-//                        count++;
-//                        qty.setText(count+"");
-//                    qty.setText(qty.getText().toString());
-//                    eachStoreTotalPrice.setText(Integer.parseInt(qty.getText().toString()) * Integer.parseInt(eachStoreTotalPrice.getText().toString()));
-////                    }
-//                }
-//            });
-//
-//            //-버튼튼
-//            mdMinusBtn.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    if (count == 1){
-//                        Toast.makeText(v.getContext(), "1 세트 이상의 수를 입력해주세요.", Toast.LENGTH_SHORT).show();
-//                    }
-//                    else{
-//                        count--;
-//                        qty.setText(count+"");
-//                        qty.setText(qty.getText().toString());
-//                        eachStoreTotalPrice.setText(Integer.parseInt(qty.getText().toString()) * Integer.parseInt(eachStoreTotalPrice.getText().toString()));
-//                    }
-//                }
-//            });
+                            Call<ResponseBody> call2 = service.cartUpdate(count, user_id, store_id_pos, md_id_pos);
+                            call2.enqueue(new Callback<ResponseBody>() {
+                                @Override
+                                public void onResponse(Call<ResponseBody> call2, Response<ResponseBody> response2) {
+                                    try {
+                                        JsonObject res = (JsonObject) jsonParser.parse(response2.body().string());
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                @Override
+                                public void onFailure(Call<ResponseBody> call2, Throwable t) {
+                                    Log.e(TAG, "onFailure: e " + t.getMessage());
+                                }
+                            });
+                        }
+                        @Override
+                        public void onFailure(Call<ResponseBody> call2, Throwable t) {
+                            Log.e(TAG, "onFailure: e " + t.getMessage());
+                        }
+                    });
+                }
+            });
+
+            mdMinusBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = getAdapterPosition();
+                    if (pos!=RecyclerView.NO_POSITION) {
+                        if (mListener != null) {
+                            pos_item = pos;
+                        }
+                    }
+                    Call<ResponseBody> call = service.cartChecked(user_id, pos);
+                    call.enqueue(new Callback<ResponseBody>() {
+                        @Override
+                        public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                            int count = 0;
+                            String store_id_pos = "", md_id_pos= "";
+                            try {
+                                JsonObject res = (JsonObject) jsonParser.parse(response.body().string());
+                                cart_checked = res.get("cart_checked").getAsJsonArray();
+                                count = cart_checked.get(0).getAsJsonObject().get("select_qty").getAsInt();
+                                int each_price = cart_checked.get(0).getAsJsonObject().get("pay_price").getAsInt();
+                                store_id_pos = cart_checked.get(0).getAsJsonObject().get("store_id").getAsString();
+                                md_id_pos = cart_checked.get(0).getAsJsonObject().get("md_id").getAsString();
+
+                                if (count == 1){
+                                    Toast.makeText(v.getContext(), "1 세트 이상의 수를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                                }
+                                else{
+                                    count--;
+                                    qty.setText(count+"");
+                                    qty.setText(qty.getText().toString());
+                                    eachStoreTotalPrice.setText(String.valueOf(count * each_price));
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            Call<ResponseBody> call2 = service.cartUpdate(count, user_id, store_id_pos, md_id_pos);
+                            call2.enqueue(new Callback<ResponseBody>() {
+                                @Override
+                                public void onResponse(Call<ResponseBody> call2, Response<ResponseBody> response2) {
+                                    try {
+                                        JsonObject res = (JsonObject) jsonParser.parse(response2.body().string());
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                @Override
+                                public void onFailure(Call<ResponseBody> call2, Throwable t) {
+                                    Log.e(TAG, "onFailure: e " + t.getMessage());
+                                }
+                            });
+                        }
+                        @Override
+                        public void onFailure(Call<ResponseBody> call, Throwable t) {
+                            Log.e(TAG, "onFailure: e " + t.getMessage());
+                        }
+
+                    });
+                }
+            });
         }
     }
 
@@ -305,7 +329,7 @@ public class CartListAdapter extends RecyclerView.Adapter<CartListAdapter.ViewHo
         store_id = ((CartListActivity)CartListActivity.mContext).store_id;
         md_id = ((CartListActivity)CartListActivity.mContext).md_id;
         Glide.with(holder.itemView).load(item.getMdImg()).into(holder.mdImg);
-        holder.eachMdPrice.setText(item.getEachMdPrice());
+//        holder.eachMdPrice.setText(item.getEachMdPrice());
         holder.eachStoreTotalPrice.setText(item.getEachStoreTotalPrice());
         holder.mdName.setText(item.getMdName());
         holder.mdSet.setText(item.getMdSet());
