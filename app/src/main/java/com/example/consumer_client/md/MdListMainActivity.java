a80eb0bc026022520b8d946849e4f303bbdbb2d3
leejw-lu <jiwoojenny@naver.com>
2023-03-01 03:29:37 +0900

    #53 [기능추가] 진행중인 제품- 기준 주소지 기반 거리순 정렬

--- app/src/main/java/com/example/consumer_client/md/MdListMainActivity.java
+++ app/src/main/java/com/example/consumer_client/md/MdListMainActivity.java
@@ -2,6 +2,7 @@ package com.example.consumer_client.md;
 
 import static com.example.consumer_client.address.LocationDistance.distance;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Address;
@@ -9,24 +10,22 @@ import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.consumer_client.CustomSpinnerAdapter;
 import com.example.consumer_client.R;
 import com.example.consumer_client.farm.FarmDetailAdapter;
-import com.example.consumer_client.home.HomeProductItem;
-import com.example.consumer_client.store.StoreDetailActivity;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-
-import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -34,6 +33,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -70,6 +70,12 @@ public class MdListMainActivity extends AppCompatActivity {
 
     ArrayList<String> md_id_list = new ArrayList<String>();
 
+    private List<String> list = new ArrayList<>();
+    private Spinner spinner;
+    private CustomSpinnerAdapter adapter;
+    private String selectedItem, distance_what;
+    private Double distance_std;
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_md_total_list);
@@ -90,11 +96,6 @@ public class MdListMainActivity extends AppCompatActivity {
         standard_address = intent.getStringExtra("standard_address");
         TextView myaddress = (TextView) findViewById(R.id.myaddress);
         myaddress.setText(standard_address);
-//
-//        TextView distance_500m = (TextView) findViewById(R.id.distance_500m);
-//        TextView distance_1km = (TextView) findViewById(R.id.distance_1km);
-//        TextView distance_4km = (TextView) findViewById(R.id.distance_4km);
-//        TextView distance_all = (TextView) findViewById(R.id.distance_all);
 
         final Geocoder geocoder = new Geocoder(getApplicationContext());
         List<Address> myAddr = null;
@@ -106,6 +107,17 @@ public class MdListMainActivity extends AppCompatActivity {
         Address location = myAddr.get(0);
         myTownLat= location.getLatitude();
         myTownLong = location.getLongitude();
+
+        spinner = findViewById(R.id.spinner);
+        // 스피너 안에 넣을 데이터 임의 생성
+        list.add("500m");
+        list.add("1km");
+        list.add("2km");
+        list.add("4km");
+
+        // 스피너에 붙일 어댑터 초기화
+        adapter = new CustomSpinnerAdapter(getApplicationContext(), list);
+        spinner.setAdapter(adapter);
 
         Call<ResponseBody> call = service.getMdMainData();
         call.enqueue(new Callback<ResponseBody>() {
@@ -124,49 +136,86 @@ public class MdListMainActivity extends AppCompatActivity {
                     mMdListRecyclerView.setAdapter(mMdListMainAdapter);
 
                     //세로로 세팅
-                    LinearLayoutManager linearLayoutManager = new LinearLayoutManager(mContext);
-                    linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-                    mMdListRecyclerView.setLayoutManager(linearLayoutManager);
+                    //LinearLayoutManager linearLayoutManager = new LinearLayoutManager(mContext);
+                    //linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                    //mMdListRecyclerView.setLayoutManager(linearLayoutManager);
 
-                    GridLayoutManager gridLayoutManager = new GridLayoutManager(MdListMainActivity.this, 2, GridLayoutManager.VERTICAL, true);
+                    GridLayoutManager gridLayoutManager = new GridLayoutManager(MdListMainActivity.this, 2, GridLayoutManager.VERTICAL, false);
                     mMdListRecyclerView.setLayoutManager(gridLayoutManager);
 
                     Date now = new Date();
                     SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
                     int now_int = Integer.parseInt(format.format(now));
 
+                    // 스피너 클릭 리스너
+                    spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @SuppressLint("DefaultLocale")
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            // 어댑터에서 정의한 메서드를 통해 스피너에서 선택한 아이템의 이름을 받아온다
+                            selectedItem = adapter.getItem();
+                            Toast.makeText(MdListMainActivity.this, "선택한 아이템 : " + selectedItem, Toast.LENGTH_SHORT).show();
 
-//
-//
+                            // 어댑터에서 정의하는 게 귀찮다면 아래처럼 구할 수도 있다
+                            // getItemAtPosition()의 리턴형은 Object이므로 String 캐스팅이 필요하다
+                            String otherItem = (String) spinner.getItemAtPosition(position);
+                            //Log.e(TAG, "getItemAtPosition() - 선택한 아이템 : " + otherItem);
+
+                            if(Objects.equals(selectedItem, "500m")){
+                                //distance_what="ki"
+                                distance_std=0.05;
+                            }else if (Objects.equals(selectedItem, "1km")){
+                                distance_std=0.1;
+                            }else if (Objects.equals(selectedItem, "2km")){
+                                distance_std=0.2;
+                            } else {
+                                distance_std=0.4;
+                            }
+
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+                            //
+                        }
+                    });
+
                     for (int i = 0; i < jsonArray.size(); i++) {
-
-                        List<Address> address = geocoder.getFromLocationName(jsonArray.get(i).getAsJsonObject().get("store_loc").getAsString(), 8);
+                        List<Address> address = null;
+                        try {
+                            address = geocoder.getFromLocationName(jsonArray.get(i).getAsJsonObject().get("store_loc").getAsString(), 8);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                         Address location = address.get(0);
                         double store_lat = location.getLatitude();
                         double store_long = location.getLongitude();
-
                         //자신이 설정한 위치와 스토어 거리 distance 구하기
                         double distanceKilo = distance(myTownLat, myTownLong, store_lat, store_long, "kilometer");
-                        double distanceMeter = distance(myTownLat, myTownLong, store_lat, store_long, "meter");
+                        //double distanceMeter = distance(myTownLat, myTownLong, store_lat, store_long, "meter");
 
-                        //if (Double.compare(1, distanceKilo) > 0) { //4km 이내 제품들만 보이기
-                            //(스토어 데이터가 많이 없으므로 0.4대신 1로 test 중, 기능은 완료)
+                        //if (Double.compare(distance_std, distanceKilo) > 0) { //4km 이내 제품들만 보이기
+                        //(스토어 데이터가 많이 없으므로 0.4대신 1로 test 중, 기능은 완료)
 
-                            md_id_list.add(jsonArray.get(i).getAsJsonObject().get("md_id").getAsString());
+                        Log.d("MdList 거리비교까지? ", String.valueOf(distance_std));
+                        //md_id_list.add(jsonArray.get(i).getAsJsonObject().get("md_id").getAsString());
+                        String realIf0 = dDay.get(i).getAsString();
+                        if (realIf0.equals("0")) realIf0 = "day";
 
-                            String realIf0 = dDay.get(i).getAsString();
-                            if (realIf0.equals("0")) realIf0 = "day";
+                        Log.d("MdList 거리비교까지? ", "202행");
 
-                            addMdList("https://ggdjang.s3.ap-northeast-2.amazonaws.com/" + jsonArray.get(i).getAsJsonObject().get("mdimg_thumbnail").getAsString(),
-                                    jsonArray.get(i).getAsJsonObject().get("md_name").getAsString(),
-                                    jsonArray.get(i).getAsJsonObject().get("store_name").getAsString(),
-                                    String.format("%.2f", distanceKilo),
-                                    jsonArray.get(i).getAsJsonObject().get("pay_price").getAsString(),
-                                    "D - " + realIf0,
-                                    pu_start.get(i).getAsString()
-                            );
-                       // }
+                        addMdList("https://ggdjang.s3.ap-northeast-2.amazonaws.com/" + jsonArray.get(i).getAsJsonObject().get("mdimg_thumbnail").getAsString(),
+                                jsonArray.get(i).getAsJsonObject().get("md_name").getAsString(),
+                                jsonArray.get(i).getAsJsonObject().get("store_name").getAsString(),
+                                String.format("%.2f", distanceKilo),
+                                jsonArray.get(i).getAsJsonObject().get("pay_price").getAsString(),
+                                "D - " + realIf0,
+                                pu_start.get(i).getAsString()
+                        );
+                        //}
                     }
+
+
 
                     //거리 가까운순으로 정렬
                     mList.sort(new Comparator<MdDetailInfo>() {
@@ -214,7 +263,8 @@ public class MdListMainActivity extends AppCompatActivity {
         mList = new ArrayList<>();
     }
 
-    public void addMdList(String mdProdImg, String prodName, String storeName, String distance, String mdPrice, String dDay, String puTime) {
+    public void addMdList(String mdProdImg,
+                          String prodName, String storeName, String distance, String mdPrice, String dDay, String puTime) {
         MdDetailInfo mdDetail = new MdDetailInfo();
         mdDetail.setProdImg(mdProdImg);
         mdDetail.setProdName(prodName);
