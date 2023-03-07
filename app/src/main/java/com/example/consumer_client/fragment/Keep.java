81651c7059642926f0e91826990213d6b176118f
plum-king <jeesoo0908@gmail.com>
2023-03-08 03:34:41 +0900

    #62 [기타] km 관련 오류 수정

--- app/src/main/java/com/example/consumer_client/fragment/Keep.java
+++ app/src/main/java/com/example/consumer_client/fragment/Keep.java
@@ -1,10 +1,15 @@
 package com.example.consumer_client.fragment;
 
+import static com.example.consumer_client.address.LocationDistance.distance;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
